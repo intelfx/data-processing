014cf4ff7a78f363cdfb621781bbca646c87d132
Ivan Shapovalov <intelfx100@gmail.com>
2014-11-06 06:41:18 +0300

    differentiator: main: replace testing main() with operational one.

--- differentiator/main.cpp
+++ differentiator/main.cpp
@@ -9,6 +9,7 @@
 #include "visitor-optimize.h"
 #include "visitor-differentiate.h"
 
+#include <iomanip>
 #include <sstream>
 
 /*
@@ -19,9 +20,6 @@ namespace {
 
 // all variables being considered
 VariableMap variables;
-
-// expression evaluation results
-data_t result_nominal;
 
 } // anonymous namespace
 
@@ -37,6 +35,39 @@ void read_variables (const char* path)
 	while (!(variable_file >> std::ws, variable_file.eof())) {
 		parse_variable (variables, variable_file);
 	}
+}
+
+void testcase_visitors (Node::Base::Ptr tree, const std::string& partial_variable)
+{
+	std::cerr << "VISITORS TESTCASE" << std::endl;
+	Node::Base::Ptr simplified = Visitor::Util::apply_transformation (tree, Visitor::Simplify (partial_variable));
+	Node::Base::Ptr optimized = Visitor::Util::apply_transformation (simplified, Visitor::Optimize());
+	Node::Base::Ptr differential = Visitor::Util::apply_transformation (optimized, Visitor::Differentiate (partial_variable));
+	Node::Base::Ptr diff_simplified = Visitor::Util::apply_transformation (differential, Visitor::Simplify (partial_variable));
+	Node::Base::Ptr diff_optimized = Visitor::Util::apply_transformation (diff_simplified, Visitor::Optimize());
+
+	Visitor::Print printer (std::cerr, false);
+
+	std::cerr << "initial:    "; tree->accept (printer); std::cerr << std::endl;
+	std::cerr << "simplified: "; simplified->accept (printer); std::cerr << std::endl;
+	std::cerr << "optimized:  "; optimized->accept (printer); std::cerr << std::endl;
+	std::cerr << "diff(" << partial_variable << "): "; differential->accept (printer); std::cerr << std::endl;
+	std::cerr << "diff.smpl.: "; diff_simplified->accept (printer); std::cerr << std::endl;
+	std::cerr << "diff.opt.:  "; diff_optimized->accept (printer); std::cerr << std::endl;
+}
+
+void simplify_tree (Node::Base::Ptr& tree, const std::string& partial_variable)
+{
+	tree = Visitor::Util::apply_transformation (tree, Visitor::Simplify (partial_variable));
+	tree = Visitor::Util::apply_transformation (tree, Visitor::Optimize());
+}
+
+Node::Base::Ptr differentiate (Node::Base::Ptr tree, const std::string& partial_variable)
+{
+	simplify_tree (tree, partial_variable);
+	tree = Visitor::Util::apply_transformation (tree, Visitor::Differentiate (partial_variable));
+	simplify_tree (tree, partial_variable);
+	return std::move (tree);
 }
 
 int main (int argc, char** argv)
@@ -66,78 +97,52 @@ int main (int argc, char** argv)
 		std::cout << " " << it->first << " = " << it->second.value << " ± " << it->second.error << std::endl;
 	}
 
-	std::cout << "string: '" << expression << "'" << std::endl;
-	std::cout << "lexems:" << std::endl;
-
-	for (std::string lexem: Lexer (expression))
-	{
-		std::cout << " '" << lexem << "'" << std::endl;
-	}
-
 	Node::Base::Ptr tree = Parser (expression, variables).parse();
-	std::cout << "tree: ";
-	tree->Dump (std::cout);
-	std::cout << std::endl;
-
-	Node::Base::Ptr tree2 = tree->clone();
-	std::cout << "tree-cloned: ";
-	tree2->Dump (std::cout);
-	std::cout << std::endl;
-
-	tree = std::move (tree2);
-	std::cout << "tree-moved: ";
-	tree->Dump (std::cout);
-	std::cout << std::endl;
-
-	Visitor::Simplify simplifier ("x");
-	Node::Base::Ptr simplified (boost::any_cast<Node::Base*> (tree->accept (simplifier)));
-	std::cout << "simpl: ";
-	simplified->Dump (std::cout);
-	std::cout << std::endl;
-
-	Visitor::Optimize optimizer;
-	Node::Base::Ptr optimized (boost::any_cast<Node::Base*> (simplified->accept (optimizer)));
-	std::cout << "opt: ";
-	optimized->Dump (std::cout);
-	std::cout << std::endl;
-
-	Visitor::Differentiate differentiator ("x");
-	Node::Base::Ptr differential (boost::any_cast<Node::Base*> (optimized->accept (differentiator)));
-	std::cout << "diff: ";
-	differential->Dump (std::cout);
-	std::cout << std::endl;
-
-	Node::Base::Ptr diff_simpl (boost::any_cast<Node::Base*> (differential->accept (simplifier)));
-	std::cout << "d.simpl: ";
-	diff_simpl->Dump (std::cout);
-	std::cout << std::endl;
-
-	Visitor::Print printer (std::cout);
-	std::cout << "tree: ";
-	tree->accept (printer);
-	std::cout << std::endl;
-
-	std::cout << "simpl: ";
-	simplified->accept (printer);
-	std::cout << std::endl;
-
-	std::cout << "opt: ";
-	optimized->accept (printer);
-	std::cout << std::endl;
-
-	std::cout << "diff: ";
-	differential->accept (printer);
-	std::cout << std::endl;
-
-	std::cout << "d.simpl: ";
-	diff_simpl->accept (printer);
-	std::cout << std::endl;
-
-	Visitor::Calculate calculator;
-	result_nominal = boost::any_cast<data_t> (optimized->accept (calculator));
+	Visitor::Print print_symbolic (std::cout, false),
+	               print_substitute (std::cout, true);
+	Visitor::Calculate calculate;
 
 	std::cout << std::endl
-	          << "Results:" << std::endl
-	          << " F    = " << result_nominal << std::endl
-	          << std::endl;
+	          << "Value:" << std::endl;
+
+	std::cout << "F = "; tree->accept (print_symbolic); std::cout << " =" << std::endl;
+	std::cout << "  = "; tree->accept (print_substitute); std::cout << " =" << std::endl;
+	std::cout << "  = " << boost::any_cast<data_t> (tree->accept (calculate)) << std::endl;
+
+	std::cout << std::endl
+	          << "Partial derivatives:" << std::endl;
+
+	Node::AdditionSubtraction::Ptr full_squared_error (new Node::AdditionSubtraction);
+
+	for (auto it = variables.begin(); it != variables.end(); ++it) {
+		Node::Variable::Ptr error (new Node::Variable (it->first, it->second, true));
+		Node::Base::Ptr derivative = differentiate (tree->clone(), it->first);
+
+		std::cout << "   " << error->pretty_name() << " = " << error->value() << std::endl;
+		std::cout << "dF/d" << it->first << " = "; derivative->accept (print_symbolic); std::cout << std::endl;
+
+		Node::Power::Ptr squared_derivative (new Node::Power);
+		squared_derivative->add_child (std::move (derivative));
+		squared_derivative->add_child (Node::Base::Ptr (new Node::Value (2)));
+
+		Node::Power::Ptr squared_error (new Node::Power);
+		squared_error->add_child (std::move (error));
+		squared_error->add_child (Node::Base::Ptr (new Node::Value (2)));
+
+		Node::MultiplicationDivision::Ptr partial_error (new Node::MultiplicationDivision);
+		partial_error->add_child (std::move (squared_derivative), false);
+		partial_error->add_child (std::move (squared_error), false);
+
+		full_squared_error->add_child (std::move (partial_error), false);
+	}
+
+	Node::Power::Ptr full_error (new Node::Power);
+	full_error->add_child (std::move (full_squared_error));
+	full_error->add_child (Node::Base::Ptr (new Node::Value (0.5)));
+
+	std::cout << std::endl
+	          << "Squared error:" << std::endl;
+	std::cout << "ΔF^2 = "; full_error->accept (print_symbolic); std::cout << " =" << std::endl;
+	std::cout << "     = "; full_error->accept (print_substitute); std::cout << " =" << std::endl;
+	std::cout << "     = " << boost::any_cast<data_t> (full_error->accept (calculate)) << std::endl;
 }
