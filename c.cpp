d58a5c1138fe68e45a1b1d200ec333cdf1ac5415
Ivan Shapovalov <intelfx100@gmail.com>
2014-12-03 17:50:30 +0300

    c: an application to generate formulas for combination counts.

---
+++ c.cpp
@@ -0,0 +1,57 @@
+#include "expr.h"
+#include <differentiator/node.h>
+#include <differentiator/visitor-calculate.h>
+#include <differentiator/visitor-print.h>
+#include <differentiator/visitor-latex.h>
+
+#include "sstream"
+
+int main (int argc, char** argv)
+{
+	if (argc < 3 || argc > 4) {
+		std::cerr << "This program takes two or three arguments." << std::endl
+		          << "Usage: " << argv[0] << " <N> <K1> [K2]" << std::endl;
+	}
+
+	data_t N;
+	int Ka[2];
+	std::istringstream (argv[1]) >> N;
+	std::istringstream (argv[2]) >> Ka[0];
+	if (argc == 4) {
+		std::istringstream (argv[3]) >> Ka[1];
+	} else {
+		Ka[1] = Ka[0];
+	}
+
+	Visitor::Print printer (std::cout, false);
+	Visitor::LaTeX::Document doc ("out.tex");
+
+	for (int K = Ka[0]; K <= Ka[1]; ++K) {
+		Node::MultiplicationDivision::Ptr top (new Node::MultiplicationDivision),
+		                                  bottom (new Node::MultiplicationDivision),
+		                                  fraction;
+
+		top->add_child (Node::Base::Ptr (new Node::Value (N)), false);
+
+		for (int i = 1; i < K; ++i) {
+			Node::AdditionSubtraction::Ptr top_term (new Node::AdditionSubtraction);
+			top_term->add_child (Node::Base::Ptr (new Node::Value (N)), false);
+			top_term->add_child (Node::Base::Ptr (new Node::Value (i)), true);
+			top->add_child (std::move (top_term), false);
+
+			bottom->add_child (Node::Base::Ptr (new Node::Value (i + 1)), false);
+		}
+
+		if (bottom->children().empty()) {
+			fraction = std::move (top);
+		} else {
+			fraction = Node::MultiplicationDivision::Ptr (new Node::MultiplicationDivision);
+			fraction->add_child (std::move (top), false);
+			fraction->add_child (std::move (bottom), true);
+		}
+
+		std::cout << "(" << N << " " << K << ") = "; fraction->accept (printer); std::cout << std::endl;
+
+		doc.print (static_cast<std::ostringstream&&> (std::ostringstream() << "\\dbinom {" << N << "} {" << K << "}").str(), fraction.get(), true, true);
+	}
+}
